9e60692b86b27e67c2215cc1c3cf1a4838cd7601
AdeptApril <paradoks@yahoo.com>
2015-12-02 18:43:05 -0600

    Mostly a mockup, but it gets through a checkout when nothing is checked out.

--- SelfCheckoutSystem/src/selfcheckoutsystem/LanguageSelectionForm.java
+++ SelfCheckoutSystem/src/selfcheckoutsystem/LanguageSelectionForm.java
@@ -5,6 +5,8 @@
  */
 package selfcheckoutsystem;
 
+import javax.swing.SwingConstants;
+
 /**
  *
  * @author mintlaptop
@@ -27,30 +29,62 @@ public class LanguageSelectionForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        language1Button = new javax.swing.JButton();
         language2Button = new javax.swing.JButton();
         language3Button = new javax.swing.JButton();
-        language1Button = new javax.swing.JButton();
         language4Button = new javax.swing.JButton();
+        language1Label = new javax.swing.JLabel();
+        language2Label = new javax.swing.JLabel();
+        language3Label = new javax.swing.JLabel();
+        language4Label = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(640, 480));
         setPreferredSize(new java.awt.Dimension(640, 480));
 
+        language1Button.setText("Language 1");
+        language1Button.setMaximumSize(new java.awt.Dimension(240, 88));
+        language1Button.setMinimumSize(new java.awt.Dimension(240, 88));
+        language1Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                language1ButtonActionPerformed(evt);
+            }
+        });
+
         language2Button.setText("Language 2");
         language2Button.setMaximumSize(new java.awt.Dimension(240, 88));
         language2Button.setMinimumSize(new java.awt.Dimension(240, 88));
+        language2Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                language2ButtonActionPerformed(evt);
+            }
+        });
 
         language3Button.setText("Language 3");
         language3Button.setMaximumSize(new java.awt.Dimension(240, 88));
         language3Button.setMinimumSize(new java.awt.Dimension(240, 88));
-
-        language1Button.setText("Language 1");
-        language1Button.setMaximumSize(new java.awt.Dimension(240, 88));
-        language1Button.setMinimumSize(new java.awt.Dimension(240, 88));
+        language3Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                language3ButtonActionPerformed(evt);
+            }
+        });
 
         language4Button.setText("Language 4");
         language4Button.setMaximumSize(new java.awt.Dimension(240, 88));
         language4Button.setMinimumSize(new java.awt.Dimension(240, 88));
+        language4Button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                language4ButtonActionPerformed(evt);
+            }
+        });
+
+        language1Label.setText("Language 1");
+
+        language2Label.setText("Language 2");
+
+        language3Label.setText("Language 3");
+
+        language4Label.setText("Language 4");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -58,25 +92,39 @@ public class LanguageSelectionForm extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(language1Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(language2Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(language3Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(language4Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(language1Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(language1Label)
+                            .addComponent(language3Label))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(language4Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(language4Label)
+                            .addComponent(language2Label)
+                            .addComponent(language2Button, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(47, 47, 47)
+                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(language2Button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(language1Button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(47, 47, 47)
+                    .addComponent(language1Label)
+                    .addComponent(language2Label))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(language1Button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(language2Button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(language3Label)
+                    .addComponent(language4Label))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(language4Button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(language3Button, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -85,6 +133,24 @@ public class LanguageSelectionForm extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void language1ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_language1ButtonActionPerformed
+        // TODO add your handling code here:
+        languageNum = 1;
+        this.setVisible(false);
+    }//GEN-LAST:event_language1ButtonActionPerformed
+
+    private void language2ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_language2ButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_language2ButtonActionPerformed
+
+    private void language3ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_language3ButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_language3ButtonActionPerformed
+
+    private void language4ButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_language4ButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_language4ButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -120,11 +186,34 @@ public class LanguageSelectionForm extends javax.swing.JFrame {
             }
         });
     }
+    
+    public void updateLang1(String text, String filename)
+    {
+        language1Label.setText(text);
+        language1Button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/selfcheckoutsystem/images/" + filename ))); // NOI18N
+    }
+    
+    public void updateLang2(String text, String filename)
+    {
+        language2Label.setText(text);
+        language2Button.setIcon(new javax.swing.ImageIcon(getClass().getResource("/selfcheckoutsystem/images/" + filename ))); // NOI18N
+        //language2Button.setVerticalTextPosition(SwingConstants.TOP);
+    }
+    
+    public int getLanguageNum()
+    {
+        return languageNum;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton language1Button;
+    private javax.swing.JLabel language1Label;
     private javax.swing.JButton language2Button;
+    private javax.swing.JLabel language2Label;
     private javax.swing.JButton language3Button;
+    private javax.swing.JLabel language3Label;
     private javax.swing.JButton language4Button;
+    private javax.swing.JLabel language4Label;
     // End of variables declaration//GEN-END:variables
+    private int languageNum;
 }
--- SelfCheckoutSystem/src/selfcheckoutsystem/SelfCheckoutSystem.java
+++ SelfCheckoutSystem/src/selfcheckoutsystem/SelfCheckoutSystem.java
@@ -5,17 +5,94 @@
  */
 package selfcheckoutsystem;
 
+import java.io.*;
+import java.awt.image.BufferedImage;
+import static java.lang.Thread.sleep;
+import javax.imageio.ImageIO;
+import javax.sound.sampled.*;
+
 /**
+ * This class controls the running of the system, and thus has relatively loose
+ * cohesion; it's running through the various things needed for checking out
  *
- * @author mintlaptop
+ * @author
  */
 public class SelfCheckoutSystem {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
+        //Now the system runs the self-checkout, never to stop.
+        //TODO: Change this so there's a shutdown user ID.
+        //while (true) //Leaving out to stop infinite loop issues.
+        {
+            //Load the LanguageSelectionForm
+            LanguageSelectionForm lang = new LanguageSelectionForm();
+            lang.setVisible(true);
+            lang.updateLang1("Actual Language 1", "ZombieIcon.jpg");
+            lang.updateLang2("Actual language 2", "PIRATEJOLLYROGER.GIF");
+            //That will choose which set of language text we'll use.
+            while (lang.isVisible())
+                sleep(500);
+            languageNum = lang.getLanguageNum();
+            System.out.println("Language #" + languageNum);
+
+            //Load the login screen
+            LoginForm login = new LoginForm();
+            login.setBackground("RobotTakeReceipt.jpg");
+            login.setVisible(true);
+            playSound("ZOMBIESCANCARD.WAV");
+            while (login.isVisible()) {
+                sleep(500);
+            }
+            userID = login.getUserID();
+            System.out.println("User ID: " + userID);
+
+            //Check the login info
+            //If login info bad, TODO
+            //If login info good
+            //Load the EnterItem screen
+            EnterItemForm item = new EnterItemForm();
+            item.setBackground("RobotTakeReceipt.jpg");
+            item.setVisible(true);
+            while (item.isVisible()) {
+                sleep(500);
+            }
+
+            //If item number good
+            //If item number bad (three ways)
+            //If "finished" pushed (or timer runs out)
+            //Load goodbye screen
+            GoodbyeForm goodbye = new GoodbyeForm();
+            goodbye.setBackground("RobotTakeReceipt.jpg");
+            goodbye.setVisible(true);
+            sleep(15000);
+            goodbye.setVisible(false);
+            //Wait
+        }
     }
-    
+
+    //Taken from http://stackoverflow.com/questions/26305/how-can-i-play-sound-in-java
+    public static synchronized void playSound(final String filename) {
+        new Thread(new Runnable() {
+            // The wrapper thread is unnecessary, unless it blocks on the
+            // Clip finishing; see comments.
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();
+                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+                            SelfCheckoutSystem.class.getClass().getResourceAsStream("/selfcheckoutsystem/sounds/" + filename));
+                    clip.open(inputStream);
+                    clip.start();
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    private static int languageNum;
+    private static String userID;
 }
